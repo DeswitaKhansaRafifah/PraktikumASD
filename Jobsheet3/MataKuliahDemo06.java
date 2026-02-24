f0b80e6f6dce59093b0e26f8e6a5fd5c04c17846
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-02-24 11:16:11 +0700

    Percobaan 3

---
+++ Jobsheet3/MataKuliahDemo06.java
@@ -0,0 +1,37 @@
+package Jobsheet3;
+import java.util.Scanner;
+
+public class MataKuliahDemo06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MataKuliah06[] arrayOfMatakuliah = new MataKuliah06[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Mata kuliah ke-" + (i + 1));
+            System.out.print("Kode          : ");
+            kode = sc.nextLine();
+            System.out.print("Nama          : ");
+            nama = sc.nextLine();
+            System.out.print("Sks           : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam    : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("------------------------------");
+
+            arrayOfMatakuliah[i] = new MataKuliah06(kode, nama, sks, jumlahJam);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode       : " + arrayOfMatakuliah[i].kode);
+            System.out.println("Nama       : " + arrayOfMatakuliah[i].nama);
+            System.out.println("Sks        : " + arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].jumlahJam);
+            System.out.println("------------------------------");
+        }
+    }
+}
