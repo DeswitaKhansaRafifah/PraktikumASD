667b99fc337f6c7630c92c17076f5730a35c8168
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-02-24 10:46:11 +0700

    Modifikasi Percobaan 2

--- Jobsheet3/MahasiswaDemo06.java
+++ Jobsheet3/MahasiswaDemo06.java
@@ -17,20 +17,15 @@ public class MahasiswaDemo06 {
             arrayOfMahasiswa[i].nama = sc.nextLine();
             System.out.print("Kelas : ");
             arrayOfMahasiswa[i].kelas = sc.nextLine();
-            System.out.print("IPK : ");
+            System.out.print("IPK   : ");
             dummy = sc.nextLine();
             arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
             System.out.println("----------------------------");
         }
-
+        
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM     : "+ arrayOfMahasiswa[i].nim);
-            System.out.println("Nama    : "+ arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas   : "+ arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : "+ arrayOfMahasiswa[i].ipk);
-            System.out.println("-----------------------------");
+            arrayOfMahasiswa[i].cetakInfo();
         }
-
     }
 }
