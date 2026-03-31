0c65310ab0f7bd72f5ecc824a343c627a4afc6fd
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-03-31 09:16:03 +0700

    Modifikasi Praktikum 2

--- Praktikum05/MahasiswaDemo06.java
+++ Praktikum05/MahasiswaDemo06.java
@@ -1,19 +1,29 @@
 package Praktikum05;
+import java.util.Scanner;
 
 public class MahasiswaDemo06 {
     public static void main(String[] args) {
-        MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
-        Mahasiswa06 m1 = new Mahasiswa06("123", "Zidan", "2A", 3.2);
-        Mahasiswa06 m2 = new Mahasiswa06("124", "Ayu",   "2A", 3.5);
-        Mahasiswa06 m3 = new Mahasiswa06("125", "Sofi",  "2A", 3.1);
-        Mahasiswa06 m4 = new Mahasiswa06("126", "Sita",  "2A", 3.9);
-        Mahasiswa06 m5 = new Mahasiswa06("127", "Miki",  "2A", 3.7);
+        Scanner sc = new Scanner(System.in);
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
+        int jmlMhs = 5;
+
+        for (int i = 0; i < jmlMhs; i++) {
+            System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa06 m = new Mahasiswa06(nim, nama, kelas, ipk);
+            list.tambah(m);
+            System.out.println();
+        }
 
         System.out.println("Data mahasiswa sebelum sorting: ");
         list.tampil();
