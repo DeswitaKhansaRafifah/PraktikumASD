342b4012aa3d9fc3429dae56ac296be6d262ce9e
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-04-21 10:04:11 +0700

    Percobaan 2

--- Jobsheet9/StackTugasMahasiswa06.java
+++ Jobsheet9/StackTugasMahasiswa06.java
@@ -74,4 +74,18 @@ public class StackTugasMahasiswa06 {
     public int hitungTugas() {
         return top + 1;
     }
+
+    public String konversiDesimalKeBinar(int nilai) {
+        StackKonversi06 stack = new StackKonversi06();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
