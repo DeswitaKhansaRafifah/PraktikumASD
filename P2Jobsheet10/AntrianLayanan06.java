8f13352d510b03499fc3e4824aea07baf1e80531
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-04-28 09:37:40 +0700

    Modifikasi Percobaan 2

--- P2Jobsheet10/AntrianLayanan06.java
+++ P2Jobsheet10/AntrianLayanan06.java
@@ -112,5 +112,14 @@ public class AntrianLayanan06 {
     public int getJumlahAntrian() {
         return size;
     }
-    
+
+    public void lihatAkhir() {
+        if (IsEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.print("Mahasiswa paling belakang: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }  
 }
