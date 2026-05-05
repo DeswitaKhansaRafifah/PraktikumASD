8583d3dfecd5c01f59a1198f6d82a1b776f50189
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-05-05 08:27:40 +0700

    Percobaan 1

---
+++ Pertemuan11/Mahasiswa06.java
@@ -0,0 +1,23 @@
+package Pertemuan11;
+
+public class Mahasiswa06 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa06() {
+
+    }
+    
+    public Mahasiswa06(String nm, String name,  String kls, double ip) {
+        this.nim = nm;
+        this.nama = name;
+        this.kelas = kls;
+        this.ipk = ip;
+    }
+
+    public void tampilInformasi() {
+        System.out.println(nama + "\t" + nim + "\t" + kelas + "\t" + ipk);
+    }
+}
