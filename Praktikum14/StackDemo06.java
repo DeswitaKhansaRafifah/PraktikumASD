f100bac600c8e655656a03a6ced8097d4fcae5a5
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-06-02 09:19:13 +0700

    Praktikum Implementasi Stack

---
+++ Praktikum14/StackDemo06.java
@@ -0,0 +1,34 @@
+package Praktikum14;
+import java.util.Stack;
+
+public class StackDemo06 {
+    public static void main(String[] args) {
+        Book06 book1 = new Book06("1234", "Dasar Pemrograman");
+        Book06 book2 = new Book06("7145", "Hafalan Shalat Delisa");
+        Book06 book3 = new Book06("3562", "Muhammad Al-Fatih");
+
+        Stack<Book06> books = new Stack<>();
+        books.push(book1);
+        books.push(book2);
+        books.push(book3);
+
+        Book06 temp = books.peek();
+
+        if (temp != null) {
+            System.out.println(temp.toString());
+        }
+
+        Book06 temp2 = books.pop();
+
+        if (temp2 != null) {
+            System.out.println(temp.toString());
+        }
+
+        for (Book06 book : books) {
+            System.out.println(book.toString());
+        }
+
+        int posisi = books.search(book2);
+        System.out.println("Buku ini berada pada posisi " + posisi);
+    }
+}
