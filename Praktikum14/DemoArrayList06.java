1242d1d64b48c2309a02903aa2d2dd8c48c2b9e4
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2026-06-02 10:07:42 +0700

    Praktikum Sorting

--- Praktikum14/DemoArrayList06.java
+++ Praktikum14/DemoArrayList06.java
@@ -1,5 +1,6 @@
 package Praktikum14;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DemoArrayList06 {
     public static void main(String[] args) {
@@ -31,6 +32,15 @@ public class DemoArrayList06 {
             System.out.println(cust.toString());
         }
 
+        ArrayList<String> daftarSiswa = new ArrayList<>();
+        daftarSiswa.add("Zainab");
+        daftarSiswa.add("Andi");
+        daftarSiswa.add("Rara");
+        Collections.sort(daftarSiswa);
+
+        System.out.println(daftarSiswa);
+
+        customers.sort((c1,c2)->c1.name.compareTo(c2.name));
 
     }
 }
